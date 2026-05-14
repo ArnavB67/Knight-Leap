e9b3f9181d2b1108aebcb5b6120b9237664168bc
Arπab <85987387+ArnavB67@users.noreply.github.com>
2026-05-14 13:48:28 +0530

    added score bar

--- Scripts/game_manager.gd
+++ Scripts/game_manager.gd
@@ -3,4 +3,4 @@ extends Node
 @onready var label_10: Label = $"../Labels/Label10"
 
 func _process(delta: float) -> void:
-	label_10.text="CONGRATS! You Have completed The Tutorial with a Score of "+str(Global.score)
+	$"../Canvas/Score".text=str(Global.score)
