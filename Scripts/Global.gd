388f425d2b4a4d6f4b02c45ec672daab35cf2258
Arπab <85987387+ArnavB67@users.noreply.github.com>
2026-05-15 19:13:20 +0530

    Added Level Transition
    made the portal sprite and added level transition through portals

--- Scripts/Global.gd
+++ Scripts/Global.gd
@@ -4,6 +4,9 @@ var KnifeCount=0
 var Health=5
 var checkpoint
 var score=0
+var current_level=1
+var LevelPath="res://Scenes/LEVELS/"
+
 
 func Reset():
 	enemy_killed = false
@@ -11,3 +14,10 @@ func Reset():
 	Health=5
 	checkpoint
 	score=0
+
+
+func NextLevel():
+	Reset()
+	current_level+=1
+	var FinalPath=LevelPath+"Level_"+str(current_level)+".tscn"
+	get_tree().change_scene_to_file(FinalPath)
